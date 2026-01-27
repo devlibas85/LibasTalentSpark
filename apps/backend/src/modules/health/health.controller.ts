d4be2b5f2039dbafff0177dd1bab0056bb3a32cf
devlibas85 <dev.libas85@gmail.com>
2026-01-27 11:49:09 +0530

    .1

--- apps/backend/src/modules/health/health.controller.ts
+++ apps/backend/src/modules/health/health.controller.ts
@@ -5,7 +5,3 @@ export const  healthCheck = (_req: any, res:any)=>{
         timeStamp :new Date().toISOString(),
     })
 }
-export const getHealth = async () => {
-  const res = await fetch("http://localhost:4000/health");
-  return res.json();
-};
